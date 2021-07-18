2db1c75fa43d435c0787221588da1043bda0f2f1
Anastasia Ermishkina <nastya.yermishkina@gmail.com>
2021-07-18 18:53:11 +0200

    First task done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,19 @@
+"use stict"
+
 function solveEquation(a, b, c) {
-  let arr;
+  arr = [];
+  let D = b ** 2 - 4 * a * c;
+  if (D === 0) {
+    let x = -b/(2 * a);
+    arr.push (x);
+  } else if (D > 0) {
+    let x1 = (-b + Math.sqrt(D) )/(2 * a);
+    let x2 = (-b - Math.sqrt(D) )/(2 * a);
+    arr.push (x1);
+    arr.push (x2);
+  } else {
+    arr = [];
+  }
   // код для задачи №1 писать здесь
   return arr; // array
 }
